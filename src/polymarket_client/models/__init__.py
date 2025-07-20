c037c999e1343d0efa03422e042dff4a31059982
xinmingzhang <xinmingzhang1@gmail.com>
2025-07-20 18:59:26 -0400

    "Refactored"

--- src/polymarket_client/models/__init__.py
+++ src/polymarket_client/models/__init__.py
@@ -3,15 +3,15 @@
 from .event import ClobReward, Event, EventList, Tag
 from .event import Market as EventMarket
 from .market import Market
+from .limit_order_request import LimitOrderRequest
 from .order import (
-    LimitOrderRequest,
     Order,
     OrderList,
-    OrderResponse,
     OrderSide,
     OrderStatus,
     OrderType,
 )
+from .order_response import OrderResponse
 from .order_book import BookLevel, OrderBook
 from .pagination import PaginatedResponse, PaginationInfo
 
